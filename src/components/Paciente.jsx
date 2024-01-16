10573cbcc96270f07b243f9ebb7b4ac2896b9167
Jramongs18 <jramongs15@gmail.com>
2024-01-15 22:46:24 -0600

    mejoras

--- src/components/Paciente.jsx
+++ src/components/Paciente.jsx
@@ -48,14 +48,14 @@ const Paciente = ({ paciente, setPaciente, eliminarPaciente }) => {
             <div className=" flex justify-between mt-10">
                 <button
                     type="button"
-                    className="py-2 px-10 bg-indigo-600 hover:bg-indigo-700 text-white font-bold uppercase rounded-md"
+                    className="py-2 md:px-10 px-2 bg-indigo-600 hover:bg-indigo-700 text-white font-bold uppercase rounded-md"
                     onClick={() => setPaciente(paciente)}
                 >Editar
                 </button>
 
                 <button
                     type="button"
-                    className="py-2 px-10 bg-red-600 hover:bg-red-700 text-white font-bold uppercase rounded-md"
+                    className="py-2 md:px-10 px-2 bg-red-600 hover:bg-red-700 text-white font-bold uppercase rounded-md"
                     onClick={handleEliminar}
                 >Eliminar
                 </button>
